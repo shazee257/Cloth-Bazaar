a0d4be1304aa80398c2bf6e6400ee704bd748a95
Muhammad Shahzad <shahcoolhot2002@hotmail.com>
2019-05-04 18:03:55 +0500

    Added New Category into Database

---
+++ ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,23 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothBazar.Database;
+
+namespace ClothBazar.Services
+{
+    public class CategoriesService
+    {
+        public void SaveCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothBazar.Entities;
+using ClothBazar.Services;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+
+
+    }
+}
